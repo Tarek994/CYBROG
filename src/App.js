036a13f7c070e349d2b39ebb9b7a8608050b885f
“tarek994” <ghali.tarek.123@hotmail.com>
2022-10-06 15:58:11 +0200

    chore(app-root)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
+import Header from "./components/Header/Header";
+import Container from "./components/Container/Container";
+import Footer from "./components/Footer/Footer";
 
-function App() {
+const App = () => {
   return (
     <div>
-      <>
-      
-      
-      
-      </>
+      <Header/>
+      <Container/>
+      <Footer/>
+    
 
     </div>
   );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,1 +1,11 @@
+import "./Header.css"
 
+const Header = () => {
+    return (
+      <div>
+        Header
+      </div>
+    );
+  }
+  
+  export default Header;
