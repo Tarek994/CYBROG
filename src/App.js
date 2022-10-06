c7db40b4ec62f719ca5640f3177484fd2f770f58
“tarek994” <ghali.tarek.123@hotmail.com>
2022-10-06 16:02:25 +0200

    add index file to import Components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-import Header from "./components/Header/Header";
-import Container from "./components/Container/Container";
-import Footer from "./components/Footer/Footer";
+import {Header, Container, Footer } from "./components/index"
 
 const App = () => {
   return (
