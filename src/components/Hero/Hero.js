0817a0fc4f80d5f584fb742d1dfb3d427402d1eb
“tarek994” <ghali.tarek.123@hotmail.com>
2022-10-08 01:08:48 +0200

    add style for hero

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,7 +1,17 @@
 import "./Hero.css";
 function Hero() {
   return (
-    <div>Hero</div>
+    <div className="hero-main">
+        <div className="hero-text">
+            <h6 className="hero-subtitle">Welcome to cybrog </h6>
+            <h4 className="hero-title"><em>Browse</em>Our popular Games Here </h4>
+            
+            <div className="main-button"> 
+            <a href="browse.html">Browse Now</a>
+
+            </div>
+        </div>
+    </div>
   )
 }
 
