1e50b26acf1d029eaee30f3ed8e1402ebfb67482
Tarek994 <ghali.tarek.123@hotmail.com>
2022-10-11 01:25:59 +0200

    add data file

--- src/sectios/MostPopular/MostPopular.js
+++ src/sectios/MostPopular/MostPopular.js
@@ -1,34 +1,29 @@
 import "./MostPopular.css"
 import {Card ,SectionHeader,SectionWrapper} from "../../components/index"
-
-import popular_01 from "../../assets/images/popular-01.jpg"
-import popular_02 from "../../assets/images/popular-02.jpg"
-import popular_03 from "../../assets/images/popular-03.jpg"
-import popular_04 from "../../assets/images/popular-04.jpg"
-import popular_05 from "../../assets/images/popular-05.jpg"
-import popular_06 from "../../assets/images/popular-06.jpg"
-import popular_07 from "../../assets/images/popular-07.jpg"
-import popular_08 from "../../assets/images/popular-08.jpg"
-
-
+import MostPopularData from "../../Data/MostPopularData"
 
 const MostPopular = () => {
+
+
+  const cards = MostPopularData.map(card => {
+    return <Card key={card.id} image={card.image}
+                 title={card.title}
+                 category={card.category}
+                 rate={card.rate}
+                 download={card.download}
+ })
+  
+
+
+
+
+
   return (
     <>
     <SectionWrapper>
-
-       <SectionHeader>Most Popular</SectionHeader>
-
+       <SectionHeader> Most Popular </SectionHeader>
         <div className="most-popular-items">
-
-            <Card image={popular_01} title="Fortnite"   category="Sandbox"    rate="3.8"  download="2.8M"/>
-            <Card image={popular_02} title="Pubg"       category="Stream-x"   rate="4.8"  download="1.1M"/>
-            <Card image={popular_03} title="Dota2"      category="Legendary"  rate="2.7"  download="5.3M"/>
-            <Card image={popular_04} title="Cs-Go "     category="Battle S"   rate="3.9"  download="6.1M"/>
-            <Card image={popular_05} title="Fortnite"   category="Sandbox"    rate="3.8"  download="2.8M"/>
-            <Card image={popular_06} title="Pubg"       category="Stream-x"   rate="4.8"  download="1.1M"/>
-            <Card image={popular_07} title="Dota2"      category="Legendary"  rate="2.7"  download="5.3M"/>
-            <Card image={popular_08} title="Cs-Go "     category="Battle S"   rate="3.9"  download="6.1M"/>
+          {cards}
         </div>
         </SectionWrapper>
     
