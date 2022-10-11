106de720c9f1aa7255c1b4c70a610394f026435c
Tarek994 <ghali.tarek.123@hotmail.com>
2022-10-11 15:53:01 +0200

    chore(app-root)

--- src/sections/GamingLibrary/GamingLibrary.js
+++ src/sections/GamingLibrary/GamingLibrary.js
@@ -1,25 +1,25 @@
 import "./GamingLibrary.css"
-import {GamingLibraryCard ,SectionHeader,SectionWrapper} from "../../components/index"
+import {Card ,GamingLibraryCard ,SectionHeader,SectionWrapper} from "../../components/index"
 import GamingLibraryData from "../../Data/GamingLibraryData"
 
 
 const GamingLibrary = () => {
 
-    // const cards = GamingLibraryData.map(card => {
-    //     return <Card key={card.id} image={card.image}
-    //                  title={card.title}
-    //                  category={card.category}
-    //                  rate={card.rate}
-    //                  download={card.download}/>
-    //  })
+    const cards = GamingLibraryData.map(card => {
+        return <Card key={card.id} image={card.image}
+                     title={card.title}
+                     category={card.category}
+                     rate={card.rate}
+                     download={card.download}/>
+     })
 
 
   return (
     <>
     <SectionWrapper>
-       <SectionHeader> GamingLibrary </SectionHeader>
+       <SectionHeader> Gaming Library </SectionHeader>
         <div className="most-popular-items">
-          {/* {cards} */}
+          {/* {cards} */} 
           <GamingLibraryCard/>
         </div>
         </SectionWrapper>
