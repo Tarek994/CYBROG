57a1e6011a84d3bcdacc668b400e8bb0a67f3fd5
Tarek994 <ghali.tarek.123@hotmail.com>
2022-12-04 02:42:53 +0200

    comment(index): comment export line of Navitem

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 export {default as Container} from "./Container/Container"
-export {default as NavItem} from "./NavItem/NavItem"
-export {NavItemDropDown } from "./NavItem/NavItem"
+// export {default as NavItem} from "./NavItem/NavItem"
+// export {NavItemDropDown } from "./NavItem/NavItem"
 export {default as  Card } from "../components/Card/Card"
 export {default as  GamingLibraryCard } from "./GamingLibraryCard/GamingLibraryCard"
 export {default as  PrimaryButton } from "./Buttons/Buttons"
