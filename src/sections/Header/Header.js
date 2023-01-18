3efa112da3cfa8a48fd2b0b44ba326cb6d462742
Tarek994 <ghali.tarek.123@hotmail.com>
2023-01-18 22:44:10 +0200

    style(Header.js): customize a new navbar by using the grid columns

--- src/sections/Header/Header.js
+++ src/sections/Header/Header.js
@@ -14,35 +14,24 @@ import Offcanvas from 'react-bootstrap/Offcanvas';
 
 const Header = () => {
     return (
-      <>
-      {['sm'].map((expand) => (
-        <Navbar key={expand}  expand={expand} className="mb-3 cybrog-navbar">
-          <Container className="main-cybrog" fluid>
-            <Link to="/CYBROG">
-              <img src={logo} alt=""/>
-            </Link>
-            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`}/>
-            <Navbar.Offcanvas
-              id={`offcanvasNavbar-expand-${expand}`}
-              aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
-              placement="end"
-            >
-              <Offcanvas.Header closeButton>
-                <Offcanvas.Title  id={`offcanvasNavbarLabel-expand-${expand}`}>
-                  CY-BROG
-                </Offcanvas.Title>
-              </Offcanvas.Header>
-              <Offcanvas.Body>
-                <Nav className="justify-content-end flex-grow-1 pe-3">
-                <Link className="navigation" to="/CYBROG">Home</Link>
-                <Link className="navigation" to="/profile">Profile</Link>
-                </Nav>
-              </Offcanvas.Body>
-            </Navbar.Offcanvas>
-          </Container>
-        </Navbar>
-      ))}
-    </>
+      <Navbar className="cybrog-navbar" expand="md">
+      <Container className="main-navbar">
+        <div >
+          <Link to="/CYBROG">
+          <img src={logo} alt=""/>
+        </Link>
+        </div>
+        <div>
+        <Navbar.Toggle className="white-text"  aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse  id="basic-navbar-nav">
+          <Nav className="me-auto ">
+            <Link className="navigation"  to="/CYBROG">Home</Link>
+            <Link className="navigation"  to="/profile">Library</Link>
+          </Nav>
+        </Navbar.Collapse>
+        </div>
+      </Container>
+    </Navbar>
     );
   }
   
